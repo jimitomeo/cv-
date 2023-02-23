ea13f99a3056ff7fbab46e024e0a6146f72dbd51
Jimena Tomeo <Jimena Tomeo>
2023-02-23 14:44:47 -0300

    con cambios agregados

---
+++ js/function.js
@@ -0,0 +1,9 @@
+document.getElementById ("botón").onclick = function () {
+    console.log ("Mostramos la experiencia al capturar el evento click");
+    document.getElementById ("botón").innerHTML = "Abogada en el ámbito público, Centro Territorial de Denuncias, Abogada en Dirección Municipal de Violencia de Género; privado en diversos estudios jurídicos y freelance, docente adscripta Derecho Civil V UNR y reemplazante en escuelas secundarias"}
+
+    document.getElementById ("botón2").onclick = function () {
+        console.log ("Capturamos el evento click y mostramos los objetivos");
+        document.getElementById ("botón2").innerHTML = "Poder desarrollarme personal y profesionalmente, trabajando en equipo y siempre aprendiendo y mejorando"}
+
+        
